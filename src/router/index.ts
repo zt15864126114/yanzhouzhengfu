14f751cfb7768d3cf7d45624599ba8c5229b8dc5
zhaoteng <1813831021@qq.com>
2025-05-17 22:29:11 +0800

    优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       component: () => import('@/views/layout/index.vue'),
-      redirect: '/dashboard',
+      redirect: '/login',
       children: [
         {
           path: 'dashboard',
